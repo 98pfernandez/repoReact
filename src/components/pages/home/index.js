cf2b31262be02184d97283a809833eed613610b6
Pablo <98pablofl@gmail.com>
2022-12-07 23:33:21 -0300

    a

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -4,6 +4,8 @@ import App from '../../../App';
 import navBar from '../../navBar';
 import '../../../App.css'
 import {getFirestore, doc, getDocs, collection} from "firebase/firestore"
+import React from 'react'
+import Select from 'react-select'
 
 
 const carritoQty = (itemsCarrito) => {
@@ -16,6 +18,9 @@ const carritoQty = (itemsCarrito) => {
 
 function Home({ incrCart }) {
   const [items, setItems]=useState(null);
+  const [categorias, setCategorias]=useState(null);
+  
+  
   useEffect(() => {
     const db = getFirestore();
     const productos =collection(db, 'productosECommerce')
@@ -24,19 +29,35 @@ function Home({ incrCart }) {
       const productosECommerce= snapProductos.docs.map((doc)=> ({id: doc.id, ...doc.data()}));
       setItems(productosECommerce);
     })
+
+    
+    const categorias =collection(db, 'categories')
+    getDocs(categorias)
+    .then((snapProductos)=>{
+      const categorias= snapProductos.docs.map((doc)=> ({value: doc.data().id, label:doc.data().name}));
+
+      setCategorias(categorias);
+    })
+
+
+    
   }, [])
-
-
-  /*useEffect(()=>{
+  
+  /* Integración de API
+  useEffect(()=>{ 
     const [objectList, setObjectList]=useState([]);
    fetch('https://fakestoreapi.com/products/category/electronics')
    .then(res=>res.json())
    .then(json=>setObjectList(json))
   },[])*/
 
+const [selects,setSelects]=useState();
+  
   return (
     
     <div className='itemListContainer'>  
+      <h1>{selects}</h1>
+     <Select value={selects} options={categorias} onChange={()=>e=>setSelects(e.target.value)}/>
       {items && items.map(product => (
         <div key={product.id} className='card'>
           <h2 className='cardTitle'>{product.title}</h2>
